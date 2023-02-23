9f735919a12e9da21f9c528723efcce713a0095c
inayatazizi <inayatazizi43@gmail.com>
2023-02-23 10:43:13 +0500

    first commit

---
+++ build/precache-manifest.9f2e6f67e341ace24b799ae85a821cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbdaf5c9b005633261615e32d31c0a8",
+    "url": "/ahmed_inayat_profile/index.html"
+  },
+  {
+    "revision": "443ae5506380867c818e",
+    "url": "/ahmed_inayat_profile/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "6493428a75c175406859",
+    "url": "/ahmed_inayat_profile/static/css/main.a79213c2.chunk.css"
+  },
+  {
+    "revision": "443ae5506380867c818e",
+    "url": "/ahmed_inayat_profile/static/js/2.6db30e6f.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/ahmed_inayat_profile/static/js/2.6db30e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6493428a75c175406859",
+    "url": "/ahmed_inayat_profile/static/js/main.2b21a717.chunk.js"
+  },
+  {
+    "revision": "2b0c886e6bf73a380811",
+    "url": "/ahmed_inayat_profile/static/js/runtime-main.815e349f.js"
+  }
+]);
